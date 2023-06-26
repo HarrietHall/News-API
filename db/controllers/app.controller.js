326a932de44e15a33bca02ff77147f6f83c5fda3
Harriet <harriethall@github.com[200~git config --global user.email you@example.comgit config --global user.email harriethall@github.com>
2023-06-26 21:31:43 +0100

    create test file testing api/topics url

---
+++ db/controllers/app.controller.js
@@ -0,0 +1,10 @@
+const db = require("../connection");
+const { selectAllTopics } = require("../models/app.model");
+
+exports.getAllTopics = (req, res, next) => {
+  selectAllTopics()
+    .then((topics) => {
+      res.status(200).send({ topics })
+    })
+    .catch(next);
+};
---
+++ db/models/app.model.js
@@ -0,0 +1,8 @@
+const db = require("../connection");
+exports.selectAllTopics = () => {
+  let query = "SELECT * FROM topics ";
+
+  return db.query(query).then(({ rows }) => {
+    return rows;
+  });
+};
